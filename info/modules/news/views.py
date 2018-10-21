12abde0d862de09c68c2d6f803c65e68010b3dee
zhanghao <zhnaghao@126.cn>
2018-10-21 13:21:32 +0800

    首页右侧点击排行的新闻数据展示和图标样式控制

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, current_app, session
 
-from info.models import User
+from info import constants
+from info.models import User, News
 from info.modules.news import index_blu
 
 
@@ -20,8 +21,18 @@ def index():
         except Exception as e:
             current_app.logger.error(e)
     user_info = user.to_dict() if user else None
+
+    # 查询首页右侧的点击排行新闻数据并返回
+    news_list = list()
+    try:
+        news_list = News.query.order_by(News.create_time.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger(e)
+    news_list = [news.to_basic_dict() for news in news_list]
+
     data = {
-        "user_info": user_info
+        "user_info": user_info,
+        "news_list": news_list
     }
     return render_template("news/index.html", data=data)
 
